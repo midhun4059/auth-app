163d76077e606f3f89d33790b4e6b7f4ae067216
midhun4059 <midhunrpillai4059@gmail.com>
2024-07-25 13:42:50 +0530

    created test api routes and added controller

---
+++ api/Controllers/user.controller.js
@@ -0,0 +1,8 @@
+
+const test=(req,res)=>{
+  res.json({
+    message:"Api is working"
+  })
+}
+
+export default test;
--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import userRoutes from './Routes/user.route.js'
 import dotenv from 'dotenv'
 
 dotenv.config();
@@ -15,3 +16,5 @@ const app=express();
 app.listen(3000,()=>{
   console.log("server is running on port 3000")
 })
+
+app.use('/test',userRoutes)
